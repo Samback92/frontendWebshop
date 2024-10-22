539538c499f4054a0c54208c4f099bf19ae2b933
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-22 11:26:35 +0200

    Create StripeCheckout.tsx

---
+++ src/components/checkout/StripeCheckout.tsx
@@ -0,0 +1,14 @@
+// import { Elements } from "@stripe/react-stripe-js"
+// import CheckoutForm from "./CheckoutForm"
+// import { loadStripe } from "@stripe/stripe-js";
+
+
+// const stripePromise = loadStripe('pk_test_51OqXrwITRkRLeIiFw7GXzcCkfnG3sjv6O8pX344pLBeWmp0evITiPKcymd7cd4VzD4t1gTDfNZo2VAw0616SKYlj00FiEa37vb');
+
+// function StripeCheckout() {
+//     <Elements stripe={stripePromise}>
+//         <CheckoutForm />
+//     </Elements>
+// };
+
+// export default StripeCheckout
