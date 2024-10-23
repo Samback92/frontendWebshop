41c9a48c6c1679078b05a688221ed2fca8c914df
samsam1992 <143456029+Samback92@users.noreply.github.com>
2024-10-23 13:40:25 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,44 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import {HomePage} from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
 import StripeCheckoutNow from './components/checkout/CheckoutForm';
+import { CartProvider } from './contexts/CartContext'
 import './styling/App.css';
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <div className="App">
-                <header>
-                    <h1>Webshop</h1>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/products">Products</Link>
-                            </li>
-                            <li>
-                                <Link to="/checkout">Checkout</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-                <Routes>
-                    <Route path="/products" element={<ProductPage />} />
-                    {/* <Route path="/product/:id" element={<ProductDetail />} /> */}
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/checkout" element={<StripeCheckoutNow />} />
-                </Routes>
-            </div>
-        </Router>
+        <CartProvider>
+            <Router>
+                <div className="App">
+                    <header>
+                        <h1>Webshop</h1>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/products">Products</Link>
+                                </li>
+                                <li>
+                                    <Link to="/cart">Cart</Link>
+                                </li>
+                                <li>
+                                    <Link to="/checkout">Checkout</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <Routes>
+                        <Route path="/products" element={<ProductPage />} />
+                        <Route path="/cart" element={<CartPage />} />
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/checkout" element={<StripeCheckoutNow />} />
+                    </Routes>
+                </div>
+            </Router>
+        </CartProvider>
     );
 };
 
